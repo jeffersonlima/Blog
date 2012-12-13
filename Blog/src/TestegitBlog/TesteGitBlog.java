5211b4178cf97b76b3bb23dc9e77aad0c2af2abf
jeffersonlima <jefferson.b.lima@hotmail.com>
2012-12-13 15:22:54 -0200

    Teste

--- Blog/src/TestegitBlog/TesteGitBlog.java
+++ Blog/src/TestegitBlog/TesteGitBlog.java
@@ -2,7 +2,7 @@ package TestegitBlog;
 
 public class TesteGitBlog {
 	public static void main(String[] args) {
-		System.out.println("Teste Git Blog dia131212");
+		System.out.println("Teste Git Blog dia131212 hora 15:22");
 		
 	}
 
